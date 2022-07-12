ff89b0c153dfa00aa34c40fd7e4168c51640156b
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-12 14:23:14 +0100

    cleaned up code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# puts "You're in the seeding file"
+# new_question = Question.new( question: "Dummy Javascript question",
+#   answer: 'This is to test Javascript',
+#   category: 'Javascript',
+#   level: 'Junior'
+# )
+# new_question.save!
+# puts "created #{new_question.question}, #{new_question.answer}"
+
+# puts "You're in the seeding file"
+# new_question = Question.new( question: "Dummy Javascript question",
+#   answer: 'This is to test Javascript',
+#   category: 'Javascript',
+#   level: 'Mid-level'
+# )
+# new_question.save!
+# puts "created #{new_question.question}, #{new_question.answer}"
+
 puts "You're in the seeding file"
 new_question = Question.new( question: "How do “controllers”. “views”, “helpers”, and “models” vary in the case of Ruby?",
   answer: '<ul class="brand-list">
