a9475b5be7b5d38791a4dab04ae046a9ea44f64e
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-12 12:30:52 +0100

    root to flashcard on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :questions
-  root "questions#index"
-  get '/flashcards', to: 'questions#flashcards'
+  root "questions#flashcards"
+  # get '/flashcards', to: 'questions#flashcards'
 end
